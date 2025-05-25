a596cdf44736f4b0fb82c4d3af1f50b4a9fb27d6
ansurfen <axf593161@gmail.com>
2025-05-25 14:47:22 +0800

    feat: improve code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./experience";
 export * from "./profile";
 export * from "./tech-stack";
+export * from "./toc";
---
+++ src/constants/toc.ts
@@ -0,0 +1,8 @@
+import type { Locale } from "@/lib/i18n";
+
+export const toc: Record<Locale, string> = {
+    en_us: "On This Page",
+    zh_cn: "本页内容",
+    zh_tw: "本頁內容",
+    ja_jp: "このページの内容",
+};
